48df70b4dc64b4952bf97fea7579f23e6959cf58
Olivier Fortier <olivier_fortier@outlook.com>
2020-05-17 16:05:55 -0400

    gestion cinematique + 3e cam

---
+++ Assets/Mes-assets/Scripts/ScriptEntrerVille.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptEntrerVille : MonoBehaviour
+{
+
+    //référence a la caméra qui va survoler la ville comme cinématique
+    public GameObject cameraSurvolVille;
+
+    private void OnCollisionEnter(Collision autreObjet) {
+        if(autreObjet.gameObject.name == "joueur") {
+            //on désactive le collider pour ne pas activer cet événement plusieurs fois
+            GetComponent<BoxCollider>().enabled = false;
+            //on désactive la caméra actuelle
+            Camera.main.gameObject.SetActive(false);
+
+            //on active la caméra de cinématique
+            cameraSurvolVille.SetActive(true);
+        }
+    }
+
+    
+}
---
+++ Assets/Mes-assets/Scripts/ScriptTerminerCinematique.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTerminerCinematique : MonoBehaviour
+{
+    
+    //référence au gestionnaire de caméra
+    public GameObject gestionnaireCam;
+
+    //méthode pour retourner à la caméra normale apres la cinématique du tour de la ville
+    public void RetournerCamNormale() {
+
+        //on se sert de la méthode du gestionnaire de cmaéra pour retourner a la derniere caméra active
+        gestionnaireCam.GetComponent<GestionCameras>().ChangerCamera(gestionnaireCam.GetComponent<GestionCameras>().cameraActuelle);
+
+    }
+}
